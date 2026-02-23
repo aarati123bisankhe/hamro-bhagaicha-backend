e9c849c34f119edae737a61238b66aa30d30cff7
Aarati Thapa <aaratithapa@Aaratis-MacBook-Air.local>
2026-02-23 13:33:34 +0545

    update the usermodel file

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,31 +1,3 @@
-// import mongoose,{Document, Schema} from "mongoose";
-// import { UserType } from "../types/user.type";
-// import { required } from "zod/v4/core/util.cjs";
-
-// const userMongoSchema: Schema = new Schema(
-//     {
-//         fullName: {type:String, required:false},
-//         email: {type: String, required: true, unique: true},
-//         // username: {type: String, required: true, unique: true},
-//         password: {type: String, required: true},
-//         address: { type: String, required: true },
-//         phoneNumber: { type: String, required: true },
-//         role: {type: String, enum: ["user","admin"], default:"user"},
-        
-//     },
-//     {
-//         timestamps: true,
-//     }
-// )
-// export interface IUser extends UserType, Document {
-//     _id: mongoose.Types.ObjectId;
-//     createdAt: Date;
-//     updatedAt: Date;
-// }
-// export const UserModel = mongoose.model<IUser>("User",userMongoSchema);
-
-
-
 import mongoose, { Document, Schema } from "mongoose";
 import { UserType } from "../types/user.type";
 
