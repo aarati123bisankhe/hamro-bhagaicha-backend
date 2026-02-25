6211d098e00771ad705b30cefdeaefde00e2779b
Aarati Thapa <aaratithapa@Aaratis-MacBook-Air.local>
2026-02-25 14:36:08 +0545

    complete

--- src/routes/admin/user.route.ts
+++ src/routes/admin/user.route.ts
@@ -1,26 +1,3 @@
-// import { Router } from "express";
-// import { AdminUserController} from "../../controllers/admin/user.controller";
-// import { authorizedMiddleware, adminMiddleware } from "../../middlewares/authorized_middleware";
-// import { Request, Response } from "express";
-// import { success } from "zod";
-
-// let adminUserController = new AdminUserController();
-// const router = Router();
-
-// //test routes
-// router.get(
-//     "/test",
-//     authorizedMiddleware, adminMiddleware,
-//     (req: Request, res:Response) => {
-//         res.status(200).json({success: true, message: "welcome to admin"});
-//     }
-// )
-
-// router.post("/", adminUserController.createUser);
-// router.get("/:id", adminUserController.getOneUser);
-// export default router;
-
-
 import { Router } from "express";
 import { AdminUserController } from "../../controllers/admin/user.controller";
 import { adminMiddleware, authorizedMiddleware } from "../../middlewares/authorized_middleware";
