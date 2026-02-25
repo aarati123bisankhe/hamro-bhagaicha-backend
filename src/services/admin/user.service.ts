35e7da114cf30f2fa7be274605df3c4ed6c4ce58
Aarati Thapa <aaratithapa@Aaratis-MacBook-Air.local>
2026-02-25 14:34:22 +0545

    complete

--- src/services/admin/user.service.ts
+++ src/services/admin/user.service.ts
@@ -10,10 +10,6 @@ export class AdminUserService{
                 if(checkEmail){
                     throw new HttpError(409,"Email already in use");
                 }
-                // const checkUsername = await userRepository.getUserByUsername(userData.username);
-                // if(checkUsername){
-                //     throw new HttpError(403, "Username already in use");
-                // }
                 const hashedPassword = await bcryptjs.hash(userData.password,10);
                 userData.password = hashedPassword;
                 const newUser = await userRepository.createUser(userData);
